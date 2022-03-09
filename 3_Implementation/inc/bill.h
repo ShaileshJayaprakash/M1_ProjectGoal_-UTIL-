01473f6095a3e715325dc8daf4dca8b206949765
ShaileshJayaprakash <98817101+ShaileshJayaprakash@users.noreply.github.com>
2022-03-09 22:28:13 +0530

    Add files via upload

---
+++ 3_Implementation/inc/bill.h
@@ -0,0 +1,8 @@
+#ifndef __BILL_H__
+#define __BILL_H_
+int createAccount(int ind);
+void searchACustomer(int act_no);
+void payBill(int act_no);
+void addBalance(int act_no);
+void printAllCustomers();
+#endif
